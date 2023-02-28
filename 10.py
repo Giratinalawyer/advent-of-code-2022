c0588bcd482765f169e0fecb3d916c9ab6661447
Giratinalawyer <al.skeldon@gmail.com>
2023-02-28 12:42:47 -0500

    rename conventions pt 2

---
+++ 10.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+
+# Advent of Code 2022 Day _
+
+from copy import deepcopy
+import numpy as np
+from collections import defaultdict
+
+data = open('6.in').read()
+lines = [x for x in data.split('\n')]
