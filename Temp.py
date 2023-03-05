1929d3e55ec2dcb4f6c0a763fcee6c5410cfb319
Giratinalawyer <al.skeldon@gmail.com>
2023-03-04 23:14:44 -0500

    Day 12 Solved

--- Temp.py
+++ Temp.py
@@ -5,7 +5,17 @@
 import string
 from copy import deepcopy
 import numpy as np
-from collections import defaultdict
+from collections import defaultdict, deque
 
 data = open('.in').read()
 lines = [x for x in data.split('\n')]
+
+
+# figure out how to stop blank line at end issue: start with this, and maybe nthistle's format of file reading
+m = []
+with open('12.in', 'r') as file1:
+  for line in file1.readlines():
+      m.append(line.rstrip())
+    #   if trimmed_line: # Don't print blank lines
+    #       print(trimmed_line)
+print('\n'.join(m))
