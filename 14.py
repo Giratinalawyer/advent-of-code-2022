a9c328854e0da6c170cc20baa139ea22405eef36
Giratinalawyer <al.skeldon@gmail.com>
2023-03-07 08:47:24 -0500

    Day 15 part 1

--- 14.py
+++ 14.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 # Advent of Code 2022 Day 14
-
+# still need to learn zip, map, lambda, walrus operator
 d1,d2,d3 = [(0,1),(-1,1),(1,1)]
 import string
 from copy import deepcopy
@@ -42,47 +42,31 @@ for m in range(height*2):
     filled.add((-m+500,height+2))
 
 def sanding(cave: set, pt: int):
-    sand = deepcopy(cave)
+    cvcp = deepcopy(cave)
     i = 0
     while True:
-        space = True
-        sand.add((500,0))
-        locx = 500
-        locy = 0
-        turns = 0
-        while space:
-            turns += 1
-            if pt == 1 and locy >= height:
-                break
-            elif not((locx,locy + 1) in sand):
-                sand.add((locx,locy+1))
-                sand.remove((locx,locy))
-                locy = locy + 1
-            elif not((locx-1,locy+1) in sand):
-                sand.add((locx-1,locy+1))
-                sand.remove((locx,locy))
-                locx = locx -1
-                locy = locy +1
-            elif not((locx+1,locy+1) in sand):
-                sand.add((locx+1,locy+1))
-                sand.remove((locx,locy))
-                locx, locy = locx +1, locy +1
+        sand = (500,0)
+        while True:
+            if (sand[0],sand[1]+1) not in cvcp:
+                sand = (sand[0],sand[1]+1)
+            elif (sand[0]-1,sand[1]+1) not in cvcp:
+                sand = (sand[0]-1,sand[1]+1)
+            elif (sand[0]+1,sand[1]+1) not in cvcp:
+                sand = (sand[0]+1,sand[1]+1)
             else:
-                sand.add((locx,locy))
-                space = False
-            if pt == 2:
-                if locy == 0:
-                    break
-        if pt == 1 and locy >= height:
+                break
+        cvcp.add(sand)
+        if pt == 1 and sand[1] > height:
             break
         i += 1
-        if pt == 2 and locy == 0:
+        if sand == (500,0):
             break
-    return i,sand
+    return i,cvcp
+
+# if (pt == 1 and sand[1] >= height) or (pt == 2 and sand[1] == 0):
+#     break
 
 # Visualization stuff:
-
-#a[] is purely for visualization
 a = []
 for i in range(height+4):
     a.append([])
@@ -99,15 +83,121 @@ for i,j in enumerate(a):
 
 print("\n")
 
-# more visualization stuff
 for (m,n) in sanding(filled,1)[1]:
     if not(m,n) in filled:
         a[n-min(heights)][m-min(widths)+1] = "O"
 for p,q in enumerate(a):
     print("".join(q))
-
+#_________________________________
 
 print("Answers:")
 print("part 1:", sanding(filled,1)[0])
+print("now for part 2:")
 print("part 2:", sanding(filled,2)[0])
 
+
+#JPaulson's solution - after improvements, yours is basically his
+# R = set()
+# for line in lines:
+#     prev = None
+#     for point in line.split('->'):
+#         x,y = point.split(',')
+#         x,y = int(x),int(y)
+#         if prev is not None:
+#             dx = x-prev[0]
+#             dy = y-prev[1]
+#             len_ = max(abs(dx),abs(dy))
+#             for i in range(len_+1):
+#                 xx = prev[0]+i*(1 if dx>0 else (-1 if dx<0 else 0))
+#                 yy = prev[1]+i*(1 if dy>0 else (-1 if dy<0 else 0))
+#                 R.add((xx,yy))
+#         prev = (x,y)
+
+# floor = 2+max(r[1] for r in R)
+# #print(floor)
+# lo_x = min(r[0] for r in R)-2000
+# hi_x = max(r[0] for r in R)+2000
+# for x in range(lo_x, hi_x):
+#     R.add((x,floor))
+
+# did_p1 = False
+# for t in range(1000000):
+#     rock = (500,0)
+#     while True:
+#         if rock[1]+1>=floor and (not did_p1):
+#             did_p1 = True
+#             print(t)
+#         if (rock[0],rock[1]+1) not in R:
+#             rock = (rock[0],rock[1]+1)
+#         elif (rock[0]-1,rock[1]+1) not in R:
+#             rock = (rock[0]-1, rock[1]+1)
+#         elif (rock[0]+1, rock[1]+1) not in R:
+#             rock = (rock[0]+1, rock[1]+1)
+#         else:
+#             break
+#     if rock == (500,0):
+#         print(t+1)
+#         break
+#     R.add(rock)
+
+
+# nthistle soln:
+# import string
+# from collections import defaultdict
+# #from aoc_tools import *
+# dirs = [(0,1),(1,0),(0,-1),(-1,0)]
+
+# with open(r"C:\Users\Neil\Documents\AOC2022\day14\input.txt") as f:
+#     s = f.read().strip()
+# print("\n".join(x[:60] for x in s.split("\n")[:6]))
+
+# s = s.split("\n")
+# s = [[tuple(nums(x)) for x in line.split(" -> ")] for line in s]
+# # added for pypy
+# #s = [[tuple(map(int,x.split(","))) for x in line.split(" -> ")] for line in s]
+
+# sand = (500,0)
+
+# # 0 -> air
+# # 1 -> solid
+# # 2 -> solid sand
+# grid = defaultdict(lambda : 0)
+# for line in s:
+#     for (ax,ay),(bx,by) in zip(line,line[1:]):
+#         dx = bx-ax
+#         if dx != 0:
+#             dx = dx // abs(dx)
+#         dy = by-ay
+#         if dy != 0:
+#             dy = dy // abs(dy)
+#         while (ax,ay) != (bx,by):
+#             grid[ax,ay] = 1
+#             ax += dx
+#             ay += dy
+#         grid[ax,ay] = 1
+
+# maxy = max(y for x,y in grid)
+
+# for x in range(-1000,1000):
+#     grid[x,maxy+2] = 1
+
+# part = 2
+
+# sx,sy = sand
+# while True:
+#     blocked = True
+#     for dx,dy in ((0,1),(-1,1),(1,1)):
+#         if grid[(sx+dx,sy+dy)] == 0:
+#             sx += dx
+#             sy += dy
+#             blocked = False
+#             break
+#     if part == 1 and sy > maxy:
+#         break
+#     if blocked:
+#         grid[sx,sy] = 2
+#         if (sx,sy) == sand:
+#             break
+#         sx,sy = sand
+
+# print(sum(1 for v in grid.values() if v == 2))
---
+++ 15.py
@@ -0,0 +1,86 @@
+#! /usr/bin/env python3
+
+# Advent of Code 2022 Day 15
+
+dirs = [(0,1),(1,0),(0,-1),(-1,0)]
+import string
+from copy import deepcopy
+import math
+import numpy as np
+from collections import defaultdict, deque
+from functools import cmp_to_key
+
+data = open('15.in').read().strip()
+lines = [x for x in data.split('\n')]
+
+pairs = dict()
+
+for line in lines:
+    line = line.split(" ")
+    sensor = (int(line[2][2:-1]),int(line[3][2:-1]))
+    beacon = (int(line[-2][2:-1]),int(line[-1][2:]))
+    pairs[sensor] = beacon
+
+x_min = min(min([k[0] for k in pairs.keys()]),min([v[0] for v in pairs.values()]))
+x_max = max(max([k[0] for k in pairs.keys()]),max([v[0] for v in pairs.values()]))
+y_min = min(min([k[1] for k in pairs.keys()]),min([v[1] for v in pairs.values()]))
+y_max = max(max([k[1] for k in pairs.keys()]),max([v[1] for v in pairs.values()]))
+print(x_min,x_max,y_min,y_max)
+row = 2000000 #change for example input!
+
+def taxicab(src, dest):
+    return abs(src[0]-dest[0])+abs(src[1]-dest[1])
+
+max_d = max([taxicab(k,pairs[k]) for k in pairs.keys()])
+print("max_d:",max_d)
+
+beacset = set()
+for v in pairs.values():
+    beacset.add(v)
+pos = 0
+for x in range(x_min - max_d, x_max+max_d+1, 1): #includes going further in dirxns that edgemost beacons/sensors!
+    further = 0
+    for k in pairs.keys():
+        if taxicab((x,row),k) <= taxicab(k,pairs[k]):
+            # print((x,row))
+            # print("source:",k)
+            further = 1
+            break
+    if further and (x,row) not in beacset:
+        pos += 1
+print("number of cases:",pos)
+
+#  and (x,row) not in pairs.values()
+
+# data = open("15.in").read().strip()
+# lines = [x for x in data.split('\n')]
+
+# S = set()
+# B = set()
+# sum_d = 0
+# for line in lines:
+#     words = line.split()
+#     sx,sy = words[2],words[3]
+#     bx,by = words[8],words[9]
+#     sx = int(sx[2:-1])
+#     sy = int(sy[2:-1])
+#     bx = int(bx[2:-1])
+#     by = int(by[2:])
+#     d = abs(sx-bx) + abs(sy-by)
+#     sum_d += d
+#     S.add((sx,sy,d))
+#     B.add((bx,by))
+
+# def valid(x,y,S):
+#     for (sx,sy,d) in S:
+#         dxy = abs(x-sx)+abs(y-sy)
+#         if dxy<=d:
+#             return False
+#     return True
+
+# p1 = 0
+# for x in range(-int(1e7),int(1e7)):
+#     y = int(2e6)
+#     if not valid(x,y,S) and (x,y) not in B:
+#         p1 += 1
+# print(p1)
